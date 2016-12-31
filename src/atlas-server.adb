9994dbbcd7fc1d11bd570096946de84457e8b017
Stephane Carrez <Stephane.Carrez@gmail.com>
2016-12-31 18:23:46 +0100

    Add the setup application to configure the Atlas application

--- src/atlas-server.adb
+++ src/atlas-server.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  Atlas-server -- Application server
---  Copyright (C) 2011, 2012, 2013 Stephane Carrez
+--  Copyright (C) 2011, 2012, 2013, 2016 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -19,19 +19,37 @@ with Ada.Exceptions;
 
 with Util.Log.Loggers;
 
+with AWS.Config.Set;
 with ASF.Server.Web;
+with AWA.Setup.Applications;
 
 with Atlas.Applications;
 procedure Atlas.Server is
 
+   procedure Configure (Config : in out AWS.Config.Object);
+
    Log     : constant Util.Log.Loggers.Logger := Util.Log.Loggers.Create ("Atlas.Server");
    App     : constant Atlas.Applications.Application_Access := new Atlas.Applications.Application;
    WS      : ASF.Server.Web.AWS_Container;
+   Setup   : AWA.Setup.Applications.Application;
+
+   procedure Configure (Config : in out AWS.Config.Object) is
+   begin
+      AWS.Config.Set.Input_Line_Size_Limit (1_000_000);
+      AWS.Config.Set.Max_Connection (Config, 2);
+      AWS.Config.Set.Accept_Queue_Size (Config, 100);
+      AWS.Config.Set.Send_Buffer_Size (Config, 128 * 1024);
+      AWS.Config.Set.Upload_Size_Limit (Config, 100_000_000);
+   end Configure;
+
 begin
+   WS.Configure (Configure'Access);
+WS.Start;
+   Setup.Setup ("atlas.properties", WS);
    Atlas.Applications.Initialize (App);
    WS.Register_Application (Atlas.Applications.CONTEXT_PATH, App.all'Access);
    Log.Info ("Connect you browser to: http://localhost:8080/atlas/index.html");
-   WS.Start;
+   --  WS.Start;
    delay 365.0 * 24.0 * 3600.0;
    App.Close;
 exception
