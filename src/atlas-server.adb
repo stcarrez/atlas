4f6e82df3995063435bc6ac69b381baf3c642309
Stephane Carrez <Stephane.Carrez@gmail.com>
2022-07-16 16:45:17 +0200

    Update to be able to use EWS or AWS server according to build configuration

--- src/atlas-server.adb
+++ src/atlas-server.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  atlas-server -- Application server
---  Copyright (C) 2011, 2012, 2013, 2016, 2017, 2018, 2019, 2020, 2021 Stephane Carrez
+--  Copyright (C) 2011 - 2022 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,9 +18,6 @@
 with Util.Log.Loggers;
 with Util.Commands;
 
-with AWS.Net.SSL;
-with Servlet.Server.Web;
-
 with AWA.Commands.Drivers;
 with AWA.Commands.Start;
 with AWA.Commands.Setup;
@@ -28,44 +25,41 @@ with AWA.Commands.Stop;
 with AWA.Commands.List;
 with AWA.Commands.Info;
 
-with ADO.Drivers;
---  with ADO.Sqlite;
+with ADO.Sqlite;
 --  with ADO.Mysql;
 --  with ADO.Postgresql;
 
 with Atlas.Applications;
+with Atlas.Init;
 procedure Atlas.Server is
 
    package Server_Commands is
      new AWA.Commands.Drivers (Driver_Name => "atlas",
-                               Container_Type => Servlet.Server.Web.AWS_Container);
+                               Container_Type => Atlas.Init.Container_Type);
 
    package List_Command is new AWA.Commands.List (Server_Commands);
    package Start_Command is new AWA.Commands.Start (Server_Commands);
    package Stop_Command is new AWA.Commands.Stop (Server_Commands);
    package Info_Command is new AWA.Commands.Info (Server_Commands);
    package Setup_Command is new AWA.Commands.Setup (Start_Command);
+   pragma Unreferenced (List_Command, Start_Command, Stop_Command, Info_Command, Setup_Command);
 
    Log       : constant Util.Log.Loggers.Logger := Util.Log.Loggers.Create ("Atlas.Server");
    App       : constant Atlas.Applications.Application_Access
      := new Atlas.Applications.Application;
-   WS        : Servlet.Server.Web.AWS_Container renames Server_Commands.WS;
+   WS        : Atlas.Init.Container_Type renames Server_Commands.WS;
    Context   : AWA.Commands.Context_Type;
    Arguments : Util.Commands.Dynamic_Argument_List;
 begin
    --  Initialize the database drivers (all of them or specific ones).
-   ADO.Drivers.Initialize;
-   --  ADO.Sqlite.Initialize;
+   --  ADO.Drivers.Initialize;
+   ADO.Sqlite.Initialize;
    --  ADO.Mysql.Initialize;
    --  ADO.Postgresql.Initialize;
+
    WS.Register_Application (Atlas.Applications.CONTEXT_PATH, App.all'Access);
 
-   if not AWS.Net.SSL.Is_Supported then
-      Log.Error ("SSL is not supported by AWS.");
-      Log.Error ("SSL is required for the OAuth2/OpenID connector to "
-                 & "connect to OAuth2/OpenID providers.");
-      Log.Error ("Please, rebuild AWS with SSL support.");
-   end if;
+   Atlas.Init.Initialize (Log);
 
    Log.Info ("Connect you browser to: http://localhost:8080{0}/index.html",
              Atlas.Applications.CONTEXT_PATH);
